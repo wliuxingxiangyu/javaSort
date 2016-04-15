d633d3b4b46cde2316cbc8c6bc15a50bf0aa457e
wliuxiangxiangyu <wliuxingxiangyu@163.com>
2016-04-15 19:53:35 +0800

    代码有问题，end=cog反推知hit-到-cog知中间要经过hot, 但end=cog反推知hot-到-cog知中间要经过dot,dog. 2条路径： hit-hot  -dot-dog-  cog hit-hot  -lot-log-  cog

--- src/classFive/TransFromBetweenTwoWords.java
+++ src/classFive/TransFromBetweenTwoWords.java
@@ -3,7 +3,7 @@ package classFive;
 import java.util.HashSet;
 import java.util.LinkedList;
 
-public class TransFromBetweenTwoWords{
+public class TransFromBetweenTwoWords{//L5P6
 
 	public static String[] getTransPath(String[] book,String from,String to){
 		if(from==null||to==null||from.length()!=to.length()){
@@ -17,11 +17,11 @@ public class TransFromBetweenTwoWords{
 		char[] fromChars=from.toCharArray();
 		char[] toChars=to.toCharArray();
 		path.add(from);
-		if(findProcess(fromChars,toChars,path,wordMap)){
+		if(findProcess(fromChars,toChars,path,wordMap)){//找到了一条路径。
 			String[] result=new String[path.size()];
 			int index=0;
 			while(!path.isEmpty()){
-				result[index++]=path.pollFirst();
+				result[index++]=path.pollFirst();//path的第一个移出后给result[index++]
 			}
 			return result;
 		}
@@ -31,31 +31,41 @@ public class TransFromBetweenTwoWords{
 	public static boolean findProcess(char[] fromChars,char[] toChars,
 			LinkedList<String> path,HashSet<String> wordMap){
 		boolean isEquelFromAndEnd=true;
-		for(int i=0;i!=fromChars.length;i++){
-			if(fromChars[i]!=toChars[i]){
+		for(int i=0;i!=fromChars.length;i++){//逐次改变from的每个位置.往to上.看是否在book中。
+			if(fromChars[i]!=toChars[i]){//不等时,要将from[i]变成to[i].
 				isEquelFromAndEnd=false;
-				char tmp=fromChars[i];
+				char tmp=fromChars[i];//取出fromChars[i]备份
 				fromChars[i]=toChars[i];
 				String from=String.valueOf(fromChars);
 				if(wordMap.contains(from)){
-					path.addLast(from);
-					if(findProcess(fromChars,toChars,path,wordMap)){
-						return true;//变成了
+					System.out.println("wordMap.contains(from)="+ from);
+					path.addLast(from);//addLast为LinkedList方法.先将from加入path进行往深度探索.
+					if(findProcess(fromChars,toChars,path,wordMap)){//fromChars变了进递归
+						return true;//变成功了
 					}else{
-						path.removeLast();
+						path.removeLast();//递归未成功时在path中删除,removeLast为LinkedList方法。
 					}
 				}
-				fromChars[i]=tmp;//换回来
+				fromChars[i]=tmp;//备份的换回来
+				System.out.println("备份的换回来"+ tmp);
 			}
 		}
 		return isEquelFromAndEnd;
 	}
 
 	public static void main(String[] args){
-		String[] book=new String[]{"DAMP", "LAMP", "LIMP", "LIME", "DAME",
-				"DAKE", "DIKE", "DIMP", "DIKP", "LIKP", "LIKE"};
-		String from="DAMP";
-		String to="LIKE";
+		String[] book=new String[]{"hot","dot","dog","lot","log"};
+		String from="hit";
+		String to="cog";
+		//---------------
+//		String[] book=new String[]{"AAA", "AAB", "ACB", "BAB", "CCB","BBB", "CCC"};
+//		String from="AAA";
+//		String to="BBB";
+//		//---------------
+//		String[] book=new String[]{"DAMP", "LAMP", "LIMP", "LIME", "DAME",
+//				"DAKE", "DIKE", "DIMP", "DIKP", "LIKP", "LIKE"};
+//		String from="DAMP";
+//		String to="LIKE";
 		String[] result=getTransPath(book,from,to);
 		if(result!=null){
 			for(String str:result){
