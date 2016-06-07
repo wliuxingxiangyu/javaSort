02bc63a3ae91ebf73b70c798f329ef002acc2294
wliuxingxiangyu <wliuxingxiangyu@163.com>
2016-06-07 21:25:11 +0800

    启动线程时一定要start，而不是run. //		t.run();//先打印pong一定,此时失去线程特性,run为普通方法. 		t.start();//先打印pong不一定

---
+++ src/javaSort/Threadrun.java
@@ -0,0 +1,19 @@
+package javaSort;
+
+public class Threadrun {
+	public static void main(String[] args){
+		Thread t=new Thread(){
+			public void run() {
+				pong();
+			}
+		};
+		
+//		t.run();//先打印pong一定,此时失去线程特性,run为普通方法.
+		t.start();//先打印pong不一定
+		System.out.println("ping");
+	}
+	
+	public static void pong() {
+		System.out.println("pong");
+	}
+}
