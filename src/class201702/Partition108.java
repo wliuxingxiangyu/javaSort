98fd333fcefb574a70b1225d7e994846b217f4b9
wliuxingxiangyu <wliuxingxiangyu@163.com>
2017-02-17 17:46:22 +0800

    newPalinList.addAll(palinList); //此步为了将上一次循环的回文字[c,c]串加在后面组成新的回文串newPalinList[a,c,c], //addAll比add功能大,可以尾加集合组成新集合. //newList=[a],addAll([c,c])后为newList=[a,c,c];

--- src/class201702/Partition108.java
+++ src/class201702/Partition108.java
@@ -2,48 +2,56 @@ package class201702;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Partition108 {
-	public static List<List<String>> partition(String s) {
-		if(s == null || s.length() == 0) return new ArrayList<List<String>>();
-		int nLen = s.length();
-		boolean[][] dp = new boolean[nLen][nLen];
-		for(int i = 0; i< nLen; i++) dp[i][i] = true;
-		for(int i = nLen - 1; i >= 0; i--)
-			for(int j = i + 1; j < nLen ; j++) {
-				if(s.charAt(i) == s.charAt(j)) {
-					if(j - i < 2 || dp[i + 1][j - 1]) dp[i][j] = true;
-				}
-			}
-		//dp数组赋值完毕，
-		List<List<String>>[] ret =(List<List<String>>[]) Array.newInstance
-				(List.class, nLen + 1);
-		ret[nLen] = new ArrayList<List<String>>();
-		ret[nLen].add(new ArrayList<String>());
-		for(int i = nLen - 1; i >= 0; i--) {
-			ret[i] = new ArrayList<List<String>>();
-			for(int j = i; j < nLen; j++) {
-				if(!dp[i][j]) continue;
-				
-				String str = s.substring(i, j + 1);
-				List<List<String>> lists = ret[j + 1];
-	            for(List cur : lists) {
-					List<String> temp = new ArrayList<String>(cur);
-					temp.add(str);//C++版的是往头插，而add是往尾追加,此处需要改动。
-					ret[i].add(temp);
+	public static List<List<String>> partition(String s) {// partition
+		int n = s.length();
+		boolean[][] isPalindrome = new boolean[n][n];
+		for (int i = 0; i < n; i++)
+			isPalindrome[i][i] = true;
+		for (int i = n - 1; i >= 0; i--) {
+			for (int j = i + 1; j < n; j++) {
+				if (s.charAt(i) == s.charAt(j)) {
+					if (j - i < 2 || isPalindrome[i + 1][j - 1])
+						isPalindrome[i][j] = true;//表明i~j是回文，
 				}
 			}
 		}
-		return ret[0];
+		List<List<String>>[] palindromes = (List<List<String>>[]) Array
+				.newInstance(List.class, n + 1);
+
+		palindromes[n] = (List) (new LinkedList<List<String>>());
+		List<String> emptyList = new LinkedList<>();
+		palindromes[n].add(emptyList);
+		for (int i = n - 1; i >= 0; i--) {
+			palindromes[i] = (List) (new LinkedList<List<String>>());
+			for (int j = i; j < n; j++) {
+				if (isPalindrome[i][j]) {
+					List<List<String>> palinLists = palindromes[j + 1];
+					String substring = s.substring(i, j + 1);//原始字符串[i]~[j+1]
+					for (List<String> palinList : palinLists) {
+						List<String> newPalinList = new LinkedList<>();
+						newPalinList.add(substring);//substring=‘c’
+						newPalinList.addAll(palinList);
+						//此步为了将上一次循环的回文字[c,c]串加在后面组成新的回文串newPalinList[a,c,c],
+						//addAll比add功能大,可以尾加集合组成新集合.
+						//newList=[a],addAll([c,c])后为newList=[a,c,c];
+						palindromes[i].add(newPalinList);
+					}
+				}
+			}
+		}
+		return palindromes[0];
 	}
 
 	public static void main(String[] args) {
-		String str="abbacc";
-		int count=0;
-		List<List<String>> res= partition(str);
+		String str = "abbacc";
+		int count = 0;
+		List<List<String>> res = partition(str);
 		for (int i = 0; i < res.size(); i++) {
-			System.out.println((count++)+","+res.get(i));
+			System.out.println((count++) + "," + res.get(i));
 		}
 		System.out.println("end");
 	}
