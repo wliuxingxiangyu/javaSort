6ac3c8b83e5aaef2fcfc654d64841e92a1170a56
wliuxingxiangyu <wliuxingxiangyu@163.com>
2017-02-17 21:50:06 +0800

    Partition108分割字符串-右上角关键图。

--- src/class201702/Partition108.java
+++ src/class201702/Partition108.java
@@ -2,7 +2,6 @@ package class201702;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Partition108 {
@@ -19,31 +18,32 @@ public class Partition108 {
 				}
 			}
 		}
-		List<List<String>>[] palindromes = (List<List<String>>[]) Array
+		List<List<String>>[] palinListLists = (List<List<String>>[]) Array
 				.newInstance(List.class, n + 1);
+		//定义如同int[] arr;这里palinListLists每个元素类型是List<List<String>>。
 
-		palindromes[n] = (List) (new LinkedList<List<String>>());
-		List<String> emptyList = new LinkedList<>();
-		palindromes[n].add(emptyList);
+		palinListLists[n] = (List) (new ArrayList<List<String>>());
+		List<String> emptyList = new ArrayList<>();
+		palinListLists[n].add(emptyList);//定义时:在最后多定义一个空list,供n-1下标的palinLists尾加上加回文串.
 		for (int i = n - 1; i >= 0; i--) {
-			palindromes[i] = (List) (new LinkedList<List<String>>());
+			palinListLists[i] = (List) (new ArrayList<List<String>>());
 			for (int j = i; j < n; j++) {
-				if (isPalindrome[i][j]) {
-					List<List<String>> palinLists = palindromes[j + 1];
-					String substring = s.substring(i, j + 1);//原始字符串[i]~[j+1]
+				if (isPalindrome[i][j]) {//原始字符串s[i]~s[j]为回文
+					List<List<String>> palinLists = palinListLists[j + 1];//j+1下标的palinLists供j下标尾加.
+					String palinStr = s.substring(i, j + 1);//原始字符串s[i]~s[j]为回文,for中搭配后面的回文,
 					for (List<String> palinList : palinLists) {
-						List<String> newPalinList = new LinkedList<>();
-						newPalinList.add(substring);//substring=‘c’
-						newPalinList.addAll(palinList);
+						List<String> newPalinList = new ArrayList<>();
+						newPalinList.add(palinStr);//palinStr=‘a’
+						newPalinList.addAll(palinList);//addAll是将旧list尾加在后面,组成一个list.
 						//此步为了将上一次循环的回文字[c,c]串加在后面组成新的回文串newPalinList[a,c,c],
-						//addAll比add功能大,可以尾加集合组成新集合.
-						//newList=[a],addAll([c,c])后为newList=[a,c,c];
-						palindromes[i].add(newPalinList);
+						//"addAll(list)"比"add(字符串)"功能大,可以尾加集合组成新集合;
+						//palinStr=[a],addAll([c,c])后为newPalinList=[a,c,c];
+						palinListLists[i].add(newPalinList);//因为有下标i控制,所以[a,c,c]总在[a,cc]前面.
 					}
 				}
 			}
 		}
-		return palindromes[0];
+		return palinListLists[0];
 	}
 
 	public static void main(String[] args) {
